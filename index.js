d9567897880689186269f5f5b378a8135e6c5e41
Om Mehta <ommehta16@icloud.com>
2025-04-30 22:54:30 -0400

    Build base website (mainly top/intro section)

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-const slider = document.getElementById("slider");
-const valueDisplay = document.getElementById("slider-value");
+const addFiller = () => {
+    const a = document.getElementsByClassName("addfiller");
 
-let waitUntil = 0;
-
-const a = (slider, display, cssvar) => {
-    display.innerText = slider.value;
-    document.documentElement.style.setProperty(cssvar, slider.value);
+    for (const item of a) {
+        for (let i = 0; i < 8; i++) item.innerHTML += `
+        <li>
+            <h3>title</h3>
+            <p>
+                FILLER TEXT
+            </p>
+        </li>`;
+    }
 }
 
-document.getElementById("slider").addEventListener("input", a.bind(false, document.getElementById("slider"), document.getElementById("slider-value"), "--weight"));
-document.getElementById("slider1").addEventListener("input", a.bind(false, document.getElementById("slider1"), document.getElementById("slider-value1"), "--wdth"));
+const flavorBox = document.getElementById("flavortext");
+const addFlavorText = () => {
+    const flavorOptions = [
+        `"a motivated junior who's interested in CS"`,
+        `a fellow human!`,
+        `a "studious" student suspiciously standing still`,
+        `[spook]`,
+        `(why's he in so many clubs)`,
+        `a <i>very</i> cooked junior who decided to take 9 APs (😭)</p>`,
+    ];
+    const curr = Math.floor(Math.random() * flavorOptions.length);
+    if (flavorOptions[curr][0] == "(") document.getElementById("gramer").innerText = "";
+    else document.getElementById("gramer").innerText = ",";
 
-valueDisplay.innerText = slider.value;
+    if (flavorOptions[curr] == "[spook]") {
+        spook();
+        return;
+    }
+    flavorBox.innerHTML = flavorOptions[curr];
+}
 
-a(document.getElementById("slider"), document.getElementById("slider-value"), "--weight");
-a(document.getElementById("slider1"), document.getElementById("slider-value1"), "--wdth");
+const spook = () => {
+    const spookInner = "he's in your home";
 
+    let expanded = "";
+    for (let i = 0; i < spookInner.length; i++) {
+        const c = spookInner[i];
+        if (c == " ") expanded += " ";
+        else expanded += `<span class="spooky" style="animation-delay:${Math.sin(i * 0.2)}s;">${c}</span>`;
+    }
+    flavorBox.innerHTML = "(" + expanded + ")";
+}
+
+
+addFiller();
+addFlavorText();
+
+addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.body.style.transform = "translate(0px,0px)";
+        document.body.style.opacity = 1;
+    }, 100);
+});
